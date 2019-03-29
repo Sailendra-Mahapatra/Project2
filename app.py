6d223ea7ee300522caeda6d27a4c8663cc86bc95
Alexander Brown <alex.brown.f@gmail.com>
2019-03-29 12:10:46 -0500

    started workbook review

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+import pandas as pd
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data/lr.sqlite"
+db = SQLAlchemy(app)
+Base = automap_base()
+Base.prepare(db.engine, reflect = True)
+Fixtures = Base.classes.fixtures
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/")
+def names():
+    start = "getting started"
+    return print(start)
+# need to map out our routes for our server side api
+
+
+if __name__ == "__main__":
+    app.run()
