30066454ada23753ca6569681edf18d62d4ac7c8
Alexander Brown <alex.brown.f@gmail.com>
2019-04-02 09:14:28 -0500

    git errorrrs!

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
-import gunicorn
+import psycopg2
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../data/china_trade.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL', 'postgres://qihnxxmilzvhzg:160ea1c5f64de1ea71647b683eb664db8ed500036b34de8f21428c35ce18bfd3@ec2-75-101-131-79.compute-1.amazonaws.com:5432/d6vd7jki79ppau')
 db = SQLAlchemy(app)
 Base = automap_base()
 Base.prepare(db.engine, reflect = True)
