673ebc0146bb8dc6e3eff17f3338a3ed58067d58
Alexander Brown <alex.brown.f@gmail.com>
2019-04-02 11:34:34 -0500

    remove pycop2

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
-import psycopg2
 
 app = Flask(__name__)
 
