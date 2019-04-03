02c72182b584660d7c7d29d2410f7096e8aa88ce
Alexander Brown <alex.brown.f@gmail.com>
2019-04-02 22:23:58 -0500

    Pie route

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
-import pandas as pd
+
 import numpy as np
+import pandas as pd
 import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
 
 app = Flask(__name__)
 
@@ -16,31 +17,78 @@ Base = automap_base()
 Base.prepare(db.engine, reflect = True)
 Imports = Base.classes.imports
 Exports = Base.classes.export
+IndImports = Base.classes.hs2import
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/commodity/{HSCODE}")
-def names():
-    stmt = db.session.query(Imports).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
+# @app.route("/import/hs2/<year>")
+# def imports():
+    
 
+#     return jsonify(hsc_data)
+
+# @app.route("/export/hs2/<year>")
+# def imports():
+    
+
+#     return jsonify(hsc_data)
+@app.route("/imports/tooltip/<hsc>/<year>")
+def imports_hsc(hsc, year):
     sel = [
         Imports.Description,
         Imports.YTDValue,
-        Imports.MoValue,
         Imports.Period,
-        Imports.HSCODE
-    ]
-    results =  session.query(*sel).filter(Imports.HSC == HSCODE).all()
+        Imports.HSC
+        ]
+    results = db.session.query(*sel).filter(Imports.HSC == hsc).filter(Imports.Period.like(f"%{year}%")).all()
+  
     hsc_data = {}
     for result in results:
-        hsc_data["Description"] = result[4]
+        hsc_data["Description"] = result[0]
+        hsc_data["Annual Value"] = result[1]
+        hsc_data["Year"] = result[2]
+        hsc_data["HSC"] = result[3]
 
     return jsonify(hsc_data)
-# need to map out our routes for our server side api
 
+@app.route("/exports/tooltip/<HSC>/<year>")
+def exports_hsc(codes, year):
+    sel = [
+        Imports.Description,
+        Imports.YTDValue,
+        Imports.Period,
+        Imports.HSC
+        ]
+    results = db.session.query(*sel).filter(Export.HSC == codes).filter(Export.Period.like(f"%{year}%")).all()
+  
+    hsc_data = {}
+    for result in results:
+        hsc_data["Description"] = result[0]
+        hsc_data["Annual Value"] = result[1]
+        hsc_data["Year"] = result[2]
+        hsc_data["HSC"] = result[3]
+
+    return jsonify(hsc_data)
+@app.route("/pie/<hsc>/<year>")   
+def pies(hsc, year):
+    sel = [
+        IndImports.Description,
+        IndImports.YTDValue,
+        IndImports.Period,
+        IndImports.HSC
+        ]
+
+    results = db.session.query(*sel).filter(IndImports.HSC == hsc).filter(IndImports.Period.like(f"%{year}%")).all()
+    hsc_ind_imports = {}
+    for result in results:
+        hsc_ind_imports["Description"] = result[0]
+        hsc_ind_imports["Annual Value"] = result[1]
+        hsc_ind_imports["Year"] = result[2]
+        hsc_ind_imports["HSC"] = result[3] 
+
+    return jsonify(hsc_ind_imports)
 
 if __name__ == "__main__":
     app.run()
