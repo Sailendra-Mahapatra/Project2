95fd76b40923cbfd1c026338732ec5592631548e
F. Ulises Barroso <fyiaam@fyiaam-mbp.dyndns.rice.edu>
2019-04-06 10:11:14 -0500

    re-uploaded Ulises folder

--- Ulises/treemap.js
+++ Ulises/treemap.js
@@ -1,13 +1,12 @@
-make_treemap("/trade_war_data_2016);
-
-
+function make_treemap(data){
     var visualization = d3plus.viz()
-    .container("#treemap")  // container DIV to hold the visualization
-    .data(d)  // data to use with the visualization
+    .container("#viz")  // container DIV to hold the visualization
+    .data(data)  // data to use with the visualization
     .type("tree_map")   // visualization type
-    .id("Description")         // key for which our data is unique on
+    .id("Commodity")         // key for which our data is unique on
     .size("total")      // sizing of blocks
     .draw()             // finally, draw the visualization!
 
-    })
 }
+
+
